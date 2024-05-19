92f08bd2fe5e12980b3ef3b7104eed2d8905d95d
nich <nicobaccy@icloud.com>
2024-05-19 18:45:50 +0200

    Updated boostrap lib and removed jquery lib

--- src/Blazor.SimpleModal/wwwroot/modals.js
+++ src/Blazor.SimpleModal/wwwroot/modals.js
@@ -1,11 +1,12 @@
 ﻿export function displayModal(id) {
-    // https://getbootstrap.com/docs/4.0/components/modal/#events
-    $(id).modal('show');
+    let modalInstance = bootstrap.Modal.getOrCreateInstance(id);
+    modalInstance.show();
 }
 
 export function initializeModalEvents(id, modalEventsJsHelperRef) {
-    // https://getbootstrap.com/docs/4.0/components/modal/#events
-    $(id).on('hidden.bs.modal', function (e) {
+    // https://getbootstrap.com/docs/5.1/components/modal/#events
+    let modalDOM = document.getElementById(id);
+    modalDOM.addEventListener('hidden.bs.modal', function (e) {
         console.log(e);
         // Invokes the method stored in dotnet runtime.
         modalEventsJsHelperRef.invokeMethodAsync('ModalClosedAsync')
