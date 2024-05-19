bf313d75356f50eae23f3da2df3c1ef2971d5ba0
nich <nicobaccy@icloud.com>
2024-05-19 13:45:16 +0200

    Add modal event callback from js

--- src/Blazor.SimpleModal/wwwroot/modals.js
+++ src/Blazor.SimpleModal/wwwroot/modals.js
@@ -1,3 +1,20 @@
-﻿export function displayModal(id) {
+﻿export let modalEventsJsHelperRef;
+
+export function displayModal(id) {
+    // https://getbootstrap.com/docs/4.0/components/modal/#events
+    $(id).on('hidden.bs.modal', function (e) {
+        console.log(e);
+        // Invokes the method stored in dotnet runtime.
+        modalEventsJsHelperRef.invokeMethodAsync('ModalClosedAsync')
+            .then(() => {
+                console.log("[interop] Invoked ModalClosedAsync");
+            });
+    })
+    // https://getbootstrap.com/docs/4.0/components/modal/#events
     $(id).modal('show');
+}
+
+// Initializes the helper reference required to interop with dotnet.
+export function initializeModalEventsJsHelper(helperRef) {
+    modalEventsJsHelperRef = helperRef;
 }
